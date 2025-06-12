3ed732671c1d14c3392e1bcab4637facd4dbe098
ProgWrite <dimkakuz123@gmail.com>
2025-06-12 17:56:07 +0300

    feat: add NewMatchServlet, index.html and new-match.jsp

---
+++ src/main/java/TennisMatchScoreboard/NewMatchServlet.java
@@ -0,0 +1,21 @@
+package TennisMatchScoreboard;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/new-match")
+public class NewMatchServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        request.getRequestDispatcher("/WEB-INF/jsp/new-match.jsp").forward(request, response);
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/new-match.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>Новый матч</title>
+</head>
+<body>
+<h1>Новый матч</h1>
+<a href="/">Вернуться на главную страницу</a>
+</body>
+</html>
